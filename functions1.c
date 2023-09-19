7063a30ab34637a5ef277336e59256a4f8277e34
GodwinCyber <godwinchuks032@gmail.com>
2023-09-19 02:11:01 -0700

    commit

---
+++ functions1.c
@@ -0,0 +1,164 @@
+#include "main.h"
+
+/************************* PRINT UNSIGNED NUMBER *************************/
+/**
+ * printUnsignedNumber - Prints an unsigned number
+ * @args: List of arguments
+ * @outputBuffer: Buffer array to handle print
+ * @activeFlags:  Calculates active flags
+ * @fieldWidth: Field width
+ * @precision: Precision specification
+ * @sizeSpecifier: Size specifier
+ * Return: Number of characters printed.
+ */
+int printUnsignedNumber(va_list args, char outputBuffer[],
+		int activeFlags, int fieldWidth, int precision, int sizeSpecifier)
+{
+	int i = OUTPUT_BUFFER_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
+
+	num = convertSizeUnsigned(num, sizeSpecifier);
+
+	if (num == 0)
+		outputBuffer[i--] = '0';
+
+	outputBuffer[OUTPUT_BUFFER_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		outputBuffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
+
+	i++;
+
+	return (writeUnsigned(0, i, outputBuffer, activeFlags,
+				fieldWidth, precision, sizeSpecifier));
+}
+
+/************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
+/**
+ * printOctalNumber - Prints an unsigned number in octal notation
+ * @args: List of arguments
+ * @outputBuffer: Buffer array to handle print
+ * @activeFlags:  Calculates active flags
+ * @fieldWidth: Field width
+ * @precision: Precision specification
+ * @sizeSpecifier: Size specifier
+ * Return: Number of characters printed
+ */
+int printOctalNumber(va_list args, char outputBuffer[],
+		int activeFlags, int fieldWidth, int precision, int sizeSpecifier)
+{
+	int i = OUTPUT_BUFFER_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned long int initNum = num;
+
+	UNUSED(fieldWidth);
+
+	num = convertSizeUnsigned(num, sizeSpecifier);
+
+	if (num == 0)
+		outputBuffer[i--] = '0';
+
+	outputBuffer[OUTPUT_BUFFER_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		outputBuffer[i--] = (num % 8) + '0';
+		num /= 8;
+	}
+
+	if (activeFlags & F_HASH && initNum != 0)
+		outputBuffer[i--] = '0';
+
+	i++;
+
+	return (writeUnsigned(0, i, outputBuffer, activeFlags,
+				fieldWidth, precision, sizeSpecifier));
+}
+
+/************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
+/**
+ * printHexadecimalNumber - Prints an unsigned number in hexadecimal notation
+ * @args: List of arguments
+ * @outputBuffer: Buffer array to handle print
+ * @activeFlags:  Calculates active flags
+ * @fieldWidth: Field width
+ * @precision: Precision specification
+ * @sizeSpecifier: Size specifier
+ * Return: Number of characters printed
+ */
+int printHexadecimalNumber(va_list args, char outputBuffer[],
+		int activeFlags, int fieldWidth, int precision, int sizeSpecifier)
+{
+	return (printHexadecimal(args, "0123456789abcdef", outputBuffer,
+				activeFlags, 'x', fieldWidth, precision, sizeSpecifier));
+}
+
+/************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
+/**
+ * printHexadecimalNumberUpper - Prints an unsigned
+ * number in upper hexadecimal notation
+ * @args: List of arguments
+ * @outputBuffer: Buffer array to handle print
+ * @activeFlags:  Calculates active flags
+ * @fieldWidth: Field width
+ * @precision: Precision specification
+ * @sizeSpecifier: Size specifier
+ * Return: Number of characters printed
+ */
+int printHexadecimalNumberUpper(va_list args, char outputBuffer[],
+		int activeFlags, int fieldWidth, int precision, int sizeSpecifier)
+{
+	return (printHexadecimal(args, "0123456789ABCDEF", outputBuffer,
+				activeFlags, 'X', fieldWidth, precision, sizeSpecifier));
+}
+
+/************** PRINT HEX NUMBER IN LOWER OR UPPER **************/
+/**
+ * printHexadecimal - Prints a hexadecimal number in lower or upper case
+ * @args: List of arguments
+ * @mapTo: Array of values to map the number to
+ * @outputBuffer: Buffer array to handle print
+ * @activeFlags:  Calculates active flags
+ * @flagChar: Calculates active flags
+ * @fieldWidth: Field width
+ * @precision: Precision specification
+ * @sizeSpecifier: Size specifier
+ * Return: Number of characters printed
+ */
+int printHexadecimal(va_list args, char mapTo[], char outputBuffer[],
+		int activeFlags, char flagChar, int fieldWidth,
+		int precision, int sizeSpecifier)
+{
+	int i = OUTPUT_BUFFER_SIZE - 2;
+	unsigned long int num = va_arg(args, unsigned long int);
+	unsigned long int initNum = num;
+
+	UNUSED(fieldWidth);
+
+	num = convertSizeUnsigned(num, sizeSpecifier);
+
+	if (num == 0)
+		outputBuffer[i--] = '0';
+
+	outputBuffer[OUTPUT_BUFFER_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		outputBuffer[i--] = mapTo[num % 16];
+		num /= 16;
+	}
+
+	if (activeFlags & F_HASH && initNum != 0)
+	{
+		outputBuffer[i--] = flagChar;
+		outputBuffer[i--] = '0';
+	}
+
+	i++;
+
+	return (writeUnsigned(0, i, outputBuffer, activeFlags,
+				fieldWidth, precision, sizeSpecifier));
+}
---
+++ get_flags.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * getFlags - A function that calculates active flags
+ * @formatString: Formatted string in which to print the arguments
+ * @currentIndex: take a parameter.
+ * Return: Flags:
+ */
+int getFlags(const char *formatString, int *currentIndex)
+{
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int index, currentIdx;
+	int flags = 0;
+	const char FLAGS_CHARACTERS[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARRAY[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (currentIdx = *currentIndex + 1;
+			formatString[currentIdx] != '\0'; currentIdx++)
+	{
+		for (index = 0; FLAGS_CHARACTERS[index] != '\0'; index++)
+		{
+			if (formatString[currentIdx] == FLAGS_CHARACTERS[index])
+			{
+				flags |= FLAGS_ARRAY[index];
+				break;
+			}
+		}
+
+		if (FLAGS_CHARACTERS[index] == 0)
+			break;
+	}
+
+	*currentIndex = currentIdx - 1;
+
+	return (flags);
+}
